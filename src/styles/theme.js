12190bdee6f4788b0c1122225d8fbc8838f9a105
0xGabi <gabrielpk.18@gmail.com>
2020-10-13 16:03:11 -0300

    📖 DOC: small fixes

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,94 +1,97 @@
-import { createGlobalStyle, css } from 'styled-components'
+import { createGlobalStyle, css } from "styled-components";
 
 const MEDIA_WIDTHS = {
   upToSmall: 600,
   upToMedium: 960,
-  upToLarge: 1280
-}
+  upToLarge: 1280,
+};
 
-const mediaWidthTemplates = Object.keys(MEDIA_WIDTHS).reduce((accumulator, size) => {
-  accumulator[size] = (...args) => css`
-    @media (max-width: ${MEDIA_WIDTHS[size]}px) {
-      ${css(...args)}
-    }
-  `
-  return accumulator
-}, {})
+const mediaWidthTemplates = Object.keys(MEDIA_WIDTHS).reduce(
+  (accumulator, size) => {
+    accumulator[size] = (...args) => css`
+      @media (max-width: ${MEDIA_WIDTHS[size]}px) {
+        ${css(...args)}
+      }
+    `;
+    return accumulator;
+  },
+  {}
+);
 
-const white = '#FFFFFF'
-const black = '#000000'
+const white = "#FFFFFF";
+const black = "#000000";
 
-export const theme = darkMode => ({
+export const theme = (darkMode) => ({
   white,
   black,
-  textColor: darkMode ? white : '#515151',
-  invertedTextColor: darkMode ? '#515151' : white,
-  greyText: darkMode ? white : '#6C7284',
+  textColor: darkMode ? white : "#515151",
+  invertedTextColor: darkMode ? "#515151" : white,
+  greyText: darkMode ? white : "#6C7284",
 
   // for setting css on <html>
-  backgroundColor: darkMode ? '#191B1F' : '#FFF',
+  backgroundColor: darkMode ? "#191B1F" : "#FFF",
 
-  modalBackground: darkMode ? 'rgba(0,0,0,0.6)' : 'rgba(0,0,0,0.5)',
-  cardBG: darkMode ? 'rgba(255, 255, 255, 0.1)' : 'rgba(0, 0, 0, 0.02)',
-  menuBG: darkMode ? 'rgba(10,10,10, 0.8)' : 'rgba(255, 255, 255, 0.95)',
-  marqueeBG: darkMode ? '#010101' : '#010101',
+  modalBackground: darkMode ? "rgba(0,0,0,0.6)" : "rgba(0,0,0,0.5)",
+  cardBG: darkMode ? "rgba(255, 255, 255, 0.1)" : "rgba(0, 0, 0, 0.02)",
+  menuBG: darkMode ? "rgba(10,10,10, 0.8)" : "rgba(255, 255, 255, 0.95)",
+  marqueeBG: darkMode ? "#010101" : "#010101",
 
-  inputBackground: darkMode ? '#202124' : white,
-  placeholderGray: darkMode ? '#5F5F5F' : '#E1E1E1',
-  shadowColor: darkMode ? '#000' : '#FFF19F',
+  inputBackground: darkMode ? "#202124" : white,
+  placeholderGray: darkMode ? "#5F5F5F" : "#E1E1E1",
+  shadowColor: darkMode ? "#000" : "#FFF19F",
 
   // grays
-  concreteGray: darkMode ? '#292C2F' : '#FAFAFA',
-  mercuryGray: darkMode ? '#333333' : '#E1E1E1',
-  silverGray: darkMode ? '#737373' : '#C4C4C4',
-  chaliceGray: darkMode ? '#7B7B7B' : '#AEAEAE',
-  doveGray: darkMode ? '#C4C4C4' : '#737373',
+  concreteGray: darkMode ? "#292C2F" : "#FAFAFA",
+  mercuryGray: darkMode ? "#333333" : "#E1E1E1",
+  silverGray: darkMode ? "#737373" : "#C4C4C4",
+  chaliceGray: darkMode ? "#7B7B7B" : "#AEAEAE",
+  doveGray: darkMode ? "#C4C4C4" : "#737373",
 
   colors: {
-    blue1: darkMode ? '#FFF19F' : '#D4E4FA',
-    blue2: darkMode ? '#1966D2' : '#A9C8F5',
-    blue3: darkMode ? '#165BBB' : '#7DACF0',
-    blue4: darkMode ? '#2D47A6' : '#5190EB',
-    blue5: darkMode ? '#C4D9F8' : '#2172E5',
-    blue6: darkMode ? '#C4D9F8' : '#1A5BB6',
-    blue7: darkMode ? '#C4D9F8' : '#144489',
-    blue8: darkMode ? '#C4D9F8' : '#0E2F5E',
-    blue9: darkMode ? '#C4D9F8' : '#191B1F',
+    blue1: darkMode ? "#FFF19F" : "#D4E4FA",
+    blue2: darkMode ? "#1966D2" : "#A9C8F5",
+    blue3: darkMode ? "#165BBB" : "#7DACF0",
+    blue4: darkMode ? "#2D47A6" : "#5190EB",
+    blue5: darkMode ? "#C4D9F8" : "#2172E5",
+    blue6: darkMode ? "#C4D9F8" : "#1A5BB6",
+    blue7: darkMode ? "#C4D9F8" : "#144489",
+    blue8: darkMode ? "#C4D9F8" : "#0E2F5E",
+    blue9: darkMode ? "#C4D9F8" : "#191B1F",
 
-    grey1: darkMode ? '#191B1F' : '#F7F8FA',
-    grey2: darkMode ? '#2C2F36' : '#EDEEF2',
-    grey3: darkMode ? '#40444F' : '#CED0D9',
-    grey4: darkMode ? '#565A69' : '#888D9B',
-    grey5: darkMode ? '#6C7284' : '#6C7284',
-    grey6: darkMode ? '#888D9B' : '#565A69',
-    grey7: darkMode ? '#CED0D9' : '#40444F',
-    grey8: darkMode ? '#EDEEF2' : '#2C2F36',
-    grey9: darkMode ? '#F7F8FA' : '#191B1F',
+    grey1: darkMode ? "#191B1F" : "#F7F8FA",
+    grey2: darkMode ? "#2C2F36" : "#EDEEF2",
+    grey3: darkMode ? "#40444F" : "#CED0D9",
+    grey4: darkMode ? "#565A69" : "#888D9B",
+    grey5: darkMode ? "#6C7284" : "#6C7284",
+    grey6: darkMode ? "#888D9B" : "#565A69",
+    grey7: darkMode ? "#CED0D9" : "#40444F",
+    grey8: darkMode ? "#EDEEF2" : "#2C2F36",
+    grey9: darkMode ? "#F7F8FA" : "#191B1F",
 
-    white: '#FFFFFF',
-    black: '#000000',
+    white: "#FFFFFF",
+    black: "#000000",
 
-    green1: '#E6F3EC',
-    green2: '#27AE60',
+    green1: "#E6F3EC",
+    green2: "#27AE60",
 
-    pink1: darkMode ? '#FFF19F' : '#3ab7ab',
-    pink2: '#FF8EC4',
-    pink3: '#FFD7EA',
+    pink1: darkMode ? "#FFF19F" : "#3ab7ab",
+    pink2: "#FF8EC4",
+    pink3: "#FFD7EA",
 
-    yellow1: darkMode ? '#F3BE1E95' : '#F3BE1E',
-    yellow2: darkMode ? '#FFF4D2' : '#F7F2E3',
+    yellow1: darkMode ? "#F3BE1E95" : "#F3BE1E",
+    yellow2: darkMode ? "#FFF4D2" : "#F7F2E3",
 
-    red1: '#FF6871',
+    red1: "#FF6871",
 
-    link: darkMode ? '#FFF19F' : '#3ab7ab',
-    invertedLink: darkMode ? '#FFF19F' : '#3ab7ab'
+    link: darkMode ? "#FFF19F" : "#3ab7ab",
+    invertedLink: darkMode ? "#FFF19F" : "#3ab7ab",
   },
 
   shadows: {
-    small: '0px 2px 4px rgba(0, 0, 0, 0.1)',
-    large: '0px 6px 10px rgba(0, 0, 0, 0.15)',
+    small: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+    large: "0px 6px 10px rgba(0, 0, 0, 0.15)",
     huge:
-      ' 0px 0px 1px rgba(0, 0, 0, 0.02), 0px 4px 8px rgba(0, 0, 0, 0.02), 0px 16px 24px rgba(0, 0, 0, 0.02), 0px 24px 32px rgba(0, 0, 0, 0.02)'
+      " 0px 0px 1px rgba(0, 0, 0, 0.02), 0px 4px 8px rgba(0, 0, 0, 0.02), 0px 16px 24px rgba(0, 0, 0, 0.02), 0px 24px 32px rgba(0, 0, 0, 0.02)",
   },
 
   // media queries
@@ -101,8 +104,8 @@ export const theme = darkMode => ({
   flexRowNoWrap: css`
     display: flex;
     flex-flow: row nowrap;
-  `
-})
+  `,
+});
 
 export const GlobalStyle = createGlobalStyle`
   *, *:before, *:after {
@@ -210,7 +213,12 @@ export const GlobalStyle = createGlobalStyle`
     }
   }
 
-  code > a {
-    color: ${({ theme }) => theme.colors.black};
-  }
-`
+  code {
+    background-color: ${({ theme }) => theme.placeholderGray};
+
+    & > a {
+        color: ${({ theme }) => theme.colors.black};
+    }
+  } 
+
+`;
